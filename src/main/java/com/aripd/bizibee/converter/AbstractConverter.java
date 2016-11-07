9216a919cd3413007aecbb3d823abc431a61f2f8
aripddev <dev@aripd.com>
2016-11-07 21:09:19 +0200

    update

--- src/main/java/com/aripd/bizibee/converter/AbstractConverter.java
+++ src/main/java/com/aripd/bizibee/converter/AbstractConverter.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -12,14 +13,18 @@ import javax.faces.convert.Converter;
  * @param <T> Entity
  */
 public abstract class AbstractConverter<T extends AbstractEntity> implements Converter {
-
+    
+    static final Logger LOG = Logger.getLogger(AbstractConverter.class.getName());
+    
     private final HashMap<String, T> map = new HashMap<>();
-
+    
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        return map.get(value);
+        T e = map.get(value);
+        LOG.info(e);
+        return e;
     }
-
+    
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         if (value.equals("")) {
