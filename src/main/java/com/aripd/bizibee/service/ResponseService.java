8a844920de5ae50dd71398448e0e63daa72aa6fd
aripddev <dev@aripd.com>
2016-11-13 22:24:53 +0200

    update

--- src/main/java/com/aripd/bizibee/service/ResponseService.java
+++ src/main/java/com/aripd/bizibee/service/ResponseService.java
@@ -1,9 +1,14 @@
 package com.aripd.bizibee.service;
 
+import com.aripd.bizibee.entity.DecisionEntity;
 import com.aripd.bizibee.entity.ResponseEntity;
 import javax.ejb.Local;
 
 @Local
 public interface ResponseService extends CrudService<ResponseEntity, Long> {
+
+    public ResponseEntity findOneByDecision(DecisionEntity decision);
+
+    public void updateOrCreate(DecisionEntity selectedRecord, String toString);
 
 }
--- src/main/java/com/aripd/bizibee/service/ResponseServiceBean.java
+++ src/main/java/com/aripd/bizibee/service/ResponseServiceBean.java
@@ -1,12 +1,20 @@
 package com.aripd.bizibee.service;
 
+import com.aripd.bizibee.entity.DecisionEntity;
 import com.aripd.bizibee.entity.SimulationEntity;
 import com.aripd.bizibee.entity.ResponseEntity;
+import com.aripd.bizibee.entity.ResponseEntity_;
 import com.aripd.bizibee.entity.UserEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 @Stateless
 public class ResponseServiceBean extends CrudServiceBean<ResponseEntity, Long> implements ResponseService {
@@ -28,6 +36,35 @@ public class ResponseServiceBean extends CrudServiceBean<ResponseEntity, Long> i
 
     public ResponseServiceBean() {
         super(ResponseEntity.class);
+    }
+
+    @Override
+    public ResponseEntity findOneByDecision(DecisionEntity decision) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<ResponseEntity> cq = cb.createQuery(ResponseEntity.class);
+        Root<ResponseEntity> root = cq.from(ResponseEntity.class);
+
+        Predicate predicate = cb.equal(root.get(ResponseEntity_.decision), decision);
+        cq.where(predicate);
+
+        Query query = getEntityManager().createQuery(cq);
+        List<ResponseEntity> results = query.getResultList();
+        ResponseEntity entity = null;
+        if (!results.isEmpty()) {
+            entity = results.get(0);
+        }
+        return entity;
+    }
+
+    @Override
+    public void updateOrCreate(DecisionEntity decision, String outcome) {
+        ResponseEntity entity = this.findOneByDecision(decision);
+        if (entity != null) {
+            entity.setOutcome(outcome);
+            this.update(entity);
+        } else {
+            this.create(new ResponseEntity(decision, outcome));
+        }
     }
 
 }
