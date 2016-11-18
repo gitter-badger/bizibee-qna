b8ee82c17825464a997200b050b741b9fb3be0d8
aripddev <dev@aripd.com>
2016-11-18 23:37:59 +0200

    update

--- src/main/java/com/aripd/bizibee/model/response/ResponseConverter.java
+++ src/main/java/com/aripd/bizibee/model/response/ResponseConverter.java
@@ -25,6 +25,7 @@ public class ResponseConverter {
 
     static JsonObject convert(Response1Model model) {
         return Json.createObjectBuilder()
+                // TODO decisionchoice null ise id çağırırken nullpointer exception verir
                 .add("id", model.getDecisionchoice().getId())
                 .add("name", model.getDecisionchoice().getName())
                 .add("gm", model.getDecisionchoice().getGm())
@@ -49,6 +50,7 @@ public class ResponseConverter {
 
     static JsonObject convert(Response3Model model) {
         return Json.createObjectBuilder()
+                // TODO sku null ise id çağırırken nullpointer exception verir
                 .add("id", model.getSku().getId())
                 .add("name", model.getSku().getName())
                 .add("gm", model.getSku().getGm())
@@ -73,6 +75,7 @@ public class ResponseConverter {
 
     static JsonObject convert(Response5Model model) {
         return Json.createObjectBuilder()
+                // TODO sku null ise id çağırırken nullpointer exception verir
                 .add("sku", model.getSku().getId())
                 .add("value", model.getValue())
                 .build();
@@ -81,6 +84,7 @@ public class ResponseConverter {
     static JsonObject convert(Response6Model model) {
         return Json.createObjectBuilder()
                 .add("sku", model.getSku().getId())
+                // TODO decisionchoice null ise id çağırırken nullpointer exception verir
                 .add("decisionchoice", model.getDecisionchoice().getId())
                 .build();
     }
