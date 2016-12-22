b904e810cbf5489d9e18b9bbd42d5fed4cbadfba
aripddev <dev@aripd.com>
2016-12-22 20:56:13 +0200

    update

--- src/main/java/com/aripd/bizibee/view/DashboardView.java
+++ src/main/java/com/aripd/bizibee/view/DashboardView.java
@@ -11,14 +11,10 @@ import com.aripd.bizibee.service.UserService;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.FacesException;
-import javax.faces.application.NavigationHandler;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 @Named
 @ViewScoped
@@ -80,31 +76,6 @@ public class DashboardView implements Serializable {
     public void doUpdateSimulation(ActionEvent actionEvent) {
         simulationService.update(selectedSimulation);
         messageUtil.addGlobalInfoFlashMessage("Updated");
-    }
-
-    public void doUpdateUser(ActionEvent actionEvent) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        NavigationHandler navigationHandler = context.getApplication().getNavigationHandler();
-        String navigation = "/index.xhtml?faces-redirect=true";
-        UserEntity user = userService.getCurrentUser();
-        if (selectedUser.getUsername().equalsIgnoreCase(user.getUsername())) {
-            userService.update(selectedUser);
-            messageUtil.addGlobalInfoFlashMessage("Updated");
-        } else if (userService.isExistByUsernameExceptUsername(selectedUser.getUsername(), user.getUsername())) {
-            messageUtil.addGlobalErrorFlashMessage("Username {0} is available. Please try another one.", new Object[]{selectedUser.getUsername()});
-        } else {
-            userService.update(selectedUser);
-            messageUtil.addGlobalInfoFlashMessage("Updated");
-
-            HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-            try {
-                request.logout();
-
-                navigationHandler.handleNavigation(context, null, navigation);
-            } catch (ServletException ex) {
-                throw new FacesException(ex);
-            }
-        }
     }
 
     public UserEntity getSelectedUser() {
