ab6403932483f34b5910ccb01a466b67fc711617
aripddev <dev@aripd.com>
2016-12-23 10:34:01 +0200

    update

---
+++ src/main/java/com/aripd/bizibee/model/response/ResponseModel.java
@@ -0,0 +1,45 @@
+package com.aripd.bizibee.model.response;
+
+public class ResponseModel {
+
+    private double budget;
+    private double gm;
+    private double ms;
+    private double usg;
+
+    public ResponseModel() {
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public double getGm() {
+        return gm;
+    }
+
+    public void setGm(double gm) {
+        this.gm = gm;
+    }
+
+    public double getMs() {
+        return ms;
+    }
+
+    public void setMs(double ms) {
+        this.ms = ms;
+    }
+
+    public double getUsg() {
+        return usg;
+    }
+
+    public void setUsg(double usg) {
+        this.usg = usg;
+    }
+
+}
