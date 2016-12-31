a6508ac2e1fbcbca78c5207b474adea5f6a33f6f
aripddev <dev@aripd.com>
2016-12-31 14:12:11 +0200

    update

--- src/main/java/com/aripd/bizibee/view/ReportView.java
+++ src/main/java/com/aripd/bizibee/view/ReportView.java
@@ -7,6 +7,7 @@ import com.aripd.bizibee.entity.ResponseEntity;
 import com.aripd.bizibee.entity.SimulationEntity;
 import com.aripd.bizibee.entity.SkuEntity;
 import com.aripd.bizibee.entity.UserEntity;
+import com.aripd.bizibee.model.data.LazyUserDataModelBySimulation;
 import com.aripd.bizibee.model.response.ResponseConverter;
 import com.aripd.bizibee.service.DecisionchoiceService;
 import java.io.Serializable;
@@ -17,10 +18,26 @@ import javax.inject.Named;
 import com.aripd.bizibee.service.ResponseService;
 import com.aripd.bizibee.service.SkuService;
 import com.aripd.bizibee.service.UserService;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.faces.FacesException;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import org.primefaces.model.LazyDataModel;
 
 @Named
 @ViewScoped
@@ -28,8 +45,12 @@ public class ReportView implements Serializable {
 
     @Inject
     private UserService userService;
+    private LazyDataModel<UserEntity> lazyModelPlayer;
     private UserEntity user;
     private SimulationEntity simulation;
+
+    private Long id;
+    private UserEntity selectedPlayer;
 
     @Inject
     private ResponseService responseService;
@@ -62,6 +83,7 @@ public class ReportView implements Serializable {
     public void init() {
         user = userService.getCurrentUser();
         simulation = user.getSimulation();
+        lazyModelPlayer = new LazyUserDataModelBySimulation(userService, simulation);
 
         sales = simulation.getSalesStart();
         budget = simulation.getBudgetStart();
@@ -72,6 +94,21 @@ public class ReportView implements Serializable {
         msChange = 0;
         usg = 0;
         usgChange = 0;
+    }
+
+    public void onLoad() {
+        if (id == null) {
+            messageUtil.addGlobalErrorFlashMessage("Bad request. Please use a link from within the system.");
+            return;
+        }
+
+        selectedPlayer = userService.find(id);
+
+        if (selectedPlayer == null) {
+            messageUtil.addGlobalErrorFlashMessage("Bad request. Unknown record.");
+            return;
+        }
+
     }
 
     public String calculateScore(ResponseEntity response) {
@@ -306,6 +343,40 @@ public class ReportView implements Serializable {
         }
     }
 
+    public void doGenerateReport() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            ExternalContext externalContext = facesContext.getExternalContext();
+
+            // get the JRXML template as a stream
+            InputStream inputStream = externalContext.getResourceAsStream("/resources/reports/Report.jrxml");
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+            InputStream is_subreport = externalContext.getResourceAsStream("/resources/reports/Report_Item.jrxml");
+            JasperReport jaspersubreport = JasperCompileManager.compileReport(is_subreport);
+
+            InputStream logo = externalContext.getResourceAsStream("/resources/reports/logo.png");
+
+            Map<String, Object> jasperParameters = new HashMap();
+//            jasperParameters.put(JRParameter.REPORT_LOCALE, LocaleProvider.getLocale());
+            jasperParameters.put("SUBREPORT1", jaspersubreport);
+            jasperParameters.put("player", selectedPlayer);
+            jasperParameters.put("responses", responseService.findByUser(selectedPlayer));
+            jasperParameters.put("logo", logo);
+
+            // filling report with data from data source
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, jasperParameters);
+            // exporting process
+            File file = File.createTempFile("Report." + selectedPlayer.getUsername() + ".", ".pdf");
+            externalContext.setResponseHeader("Content-Type", "application/force-download");
+            externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+            OutputStream out = externalContext.getResponseOutputStream();
+            JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+            facesContext.responseComplete();
+        } catch (JRException | IOException ex) {
+            throw new FacesException(ex);
+        }
+    }
+
     public ResponseEntity getSelectedRecord() {
         return selectedRecord;
     }
@@ -400,6 +471,26 @@ public class ReportView implements Serializable {
 
     public void setBudgetChange(double budgetChange) {
         this.budgetChange = budgetChange;
+    }
+
+    public LazyDataModel<UserEntity> getLazyModelPlayer() {
+        return lazyModelPlayer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserEntity getSelectedPlayer() {
+        return selectedPlayer;
+    }
+
+    public void setSelectedPlayer(UserEntity selectedPlayer) {
+        this.selectedPlayer = selectedPlayer;
     }
 
 }
