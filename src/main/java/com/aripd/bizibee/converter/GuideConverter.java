5668f05a0c7ce5c28831e7504a6423b6498f5287
aripddev <dev@aripd.com>
2017-01-06 20:19:19 +0200

    update

---
+++ src/main/java/com/aripd/bizibee/converter/GuideConverter.java
@@ -0,0 +1,11 @@
+package com.aripd.bizibee.converter;
+
+import com.aripd.bizibee.entity.GuideEntity;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+public class GuideConverter extends AbstractConverter<GuideEntity> {
+
+}
