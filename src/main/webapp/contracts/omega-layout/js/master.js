1fdde570038da00f8c8defda7174c5f7b52985fa
aripddev <dev@aripd.com>
2017-01-28 23:25:12 +0100

    #12 Prevent submitting the same slot number for planogram

--- src/main/webapp/contracts/omega-layout/js/master.js
+++ src/main/webapp/contracts/omega-layout/js/master.js
@@ -1,22 +1,68 @@
 $(document).ready(function () {
 });
 
-function planogram(e, slotId, answerId) {
+$.extend({
+    distinct: function (anArray) {
+        var result = [];
+        $.each(anArray, function (i, v) {
+            if ($.inArray(v, result) == -1)
+                result.push(v);
+        });
+        return result;
+    },
+    hasEmptyElement: function (anArray) {
+        return ($.inArray("", anArray) > -1);
+    }
+});
+
+function planogram(e, answerId) {
     var img = $(e).parent().parent().next().find('img');
     var src = img.attr('src');
     var alt = img.attr('alt');
     var title = img.attr('title');
     var width = img.attr('width');
     var height = img.attr('height');
-    $('#item' + slotId).html($('<img>', {id: 'image' + answerId, src: src, alt: alt, title: title, width: width, height: height, style: 'width: ' + width + 'px; height: ' + height + 'px;'}));
+    $('#item' + e.value).html($('<img>', {id: 'image' + answerId, src: src, alt: alt, title: title, width: width, height: height, style: 'width: ' + width + 'px; height: ' + height + 'px;'}));
+    checkOnePlanogram(e);
+//    checkPlanogram();
+}
 
+function checkOnePlanogram(e) {
     var inputs = $(":input.ui-inputfield");
+
     inputs.removeClass("ui-state-error");
     inputs.filter(function (i, el) {
         return inputs.not(this).filter(function () {
             return this.value === el.value;
         }).length !== 0;
     }).addClass("ui-state-error");
+
+    var inputValues = inputs.not($(e)).map((i, el) => $(el).val());
+    $(e).removeClass("ui-state-error");
+    if ($.inArray(e.value, inputValues) > -1) {
+        $(e).addClass("ui-state-error");
+        return false;
+    }
+    return true;
+}
+
+function checkPlanogram() {
+    var inputs = $(":input.ui-inputfield");
+    var inputValues = inputs.map(function () {
+        return $(this).val();
+    }).toArray();
+    var unique = $.distinct(inputValues);
+    var hasEmptyElement = $.hasEmptyElement(inputValues);
+
+    if (inputValues.length != unique.length) {
+        alert("You can not enter the same slot number");
+        return false;
+    } else if (hasEmptyElement) {
+        alert("You can not leave it blank");
+        return false;
+    } else if (inputValues.length == unique.length && hasEmptyElement == false) {
+        return true;
+    }
 }
 
 function handleSubmitRequest(xhr, status, args, dialogId, formId, dialogWidgetVar) {
